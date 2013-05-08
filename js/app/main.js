35068f803b9e302f6791f5c9c2becaa66fdc18e5
Willy Aguirre <marti1125@gmail.com>
2013-05-08 16:46:33 -0500

    add uuid a los radion buttons

--- js/app/main.js
+++ js/app/main.js
@@ -2,12 +2,25 @@ define(["jquery","bootstrap.min"], function($) {
     
     $(function() {
 
-        $('.eliminar').click(function(){
+    	function randomUUID() {
+ 
+			var s = [], itoh = '0123456789ABCDEF';
+			for (var i = 0; i <36; i++) s[i] = Math.floor(Math.random()*0x10);
+			s[14] = 4;
+			s[19] = (s[19] & 0x3) | 0x8;
+			for (var i = 0; i <36; i++) s[i] = itoh[s[i]];
+			s[8] = s[13] = s[18] = s[23] = '-';
+			return s.join('');
+		 
+		}
+
+        $('#eliminar').click(function(){
 	  		alert('Desea eliminar la fila');
 	  	});
 
         $('#agregarFilaHorario').click(function(){
-		  	$('.horarios').append(
+        	var valor = randomUUID();
+		  	$('.horarios').prepend(
 		  			'<tr>'
 		  		+		'<td><input type="checkbox"></td>'
 		  		+		'<td><input type="checkbox"></td>'
@@ -16,10 +29,10 @@ define(["jquery","bootstrap.min"], function($) {
 		  		+		'<td><input type="checkbox"></td>'
 		  		+		'<td><input type="checkbox"></td>'
 		  		+		'<td class="thcenter">'
-		  		+			'<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>'
+		  		+			'<input type="radio" name="'+valor+'" id="optionsRadios1" value="option1" checked>'
 		  		+			'&nbsp;Ene/Feb'
 		  		+			'&nbsp;&nbsp;&nbsp;'
-		  		+			'<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">'
+		  		+			'<input type="radio" name="'+valor+'" id="optionsRadios2" value="option2">'
 		  		+			'&nbsp;Mar/Dic'
 		  		+		'</td>'
 		  		+		'<td class="thcenter">'
